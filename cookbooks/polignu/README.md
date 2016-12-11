e8098fcf4a755e6d9f91f42e117b5640ce31497b
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2016-12-11 19:34:21 -0200

    Initial setup.
    - Vagrant file (ubuntu 16.04x64);
    - chef installation (v 12.10.24) - there is a bug with newer versions at
    this moment.
    - initial chef recepie to polignu webserver with nginx default install.

---
+++ cookbooks/polignu/README.md
@@ -0,0 +1,4 @@
+# polignu
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/polignu/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: polignu
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+#
+
+package "nginx" do
+  action :install
+end
