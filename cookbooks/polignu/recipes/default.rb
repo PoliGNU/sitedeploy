1c51d7c758ed1fa12e3bf2d89e0e17af1391a4c8
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2016-12-18 18:12:12 -0200

    hhvm default install - FIX #2

--- cookbooks/polignu/recipes/default.rb
+++ cookbooks/polignu/recipes/default.rb
@@ -5,6 +5,43 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 #
 
+# Install nginx
 package "nginx" do
   action :install
 end
+
+# Install HHVM
+package 'software-properties-common' do
+  action :install
+end
+
+apt_repository "hhvm" do
+  uri           "http://dl.hhvm.com/ubuntu"
+  components    ['main']
+  distribution  'xenial'
+  key           '0x5a16e7281be7a449'
+end
+
+execute "apt-get-update" do
+    command "apt-get update"
+end
+
+package 'hhvm' do
+  action :install
+end
+
+# Install Varnish
+
+# Install MariaDB
+
+# Install letsencrypt (certbot)
+
+# Setup nginx
+
+# Setup hhvm
+
+# Setup varnish
+
+# Setup letsencrypt
+
+# Setup Drupal (polignu/poligen)
