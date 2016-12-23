54c51c95aebfc6c25b8ee0180abb53f3756fd451
Leonardo Leite <leonardofl87@gmail.com>
2016-12-23 18:33:36 -0200

    verifying port 443 is listened

--- test/integration/default/serverspec/polignu_spec.rb
+++ test/integration/default/serverspec/polignu_spec.rb
@@ -4,15 +4,22 @@ require 'serverspec'
 
 set :backend, :exec
 
-describe service('mysql') do
-  it { should be_enabled }
-  it { should be_running }
-end
+# Nginx
 
 describe service('nginx') do
   it { should be_enabled }
   it { should be_running }
 end
+
+describe port(80) do
+    it { should be_listening  }
+end
+
+describe port(443) do
+    it { should be_listening  }
+end
+
+# HHVM
 
 describe service('hhvm') do
   #it { should be_enabled }
@@ -24,15 +31,7 @@ end
 #   it { should be_running }
 # end
 
-# Nginx http
-describe port(80) do
-    it { should be_listening  }
-end
 
-# Nginx https
-# describe port(443) do
-#     it { should be_listening  }
-# end
 #
 # Varnish
 # describe port(8180) do
