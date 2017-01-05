a8b263ce9b4c51b114641bc20876eba5dc8b4acd
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2017-01-05 11:05:25 -0200

    Setting up HHVM as a socket server, and with some tweaks
    FIX #14
    
    Signed-off-by: Diego Rabatone Oliveira <diraol@diraol.eng.br>

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,8 +9,16 @@
 
 user = node['linux_user']
 polignu_folder = "/home/#{user}/polignu"
+confs_folder = "#{polignu_folder}/confs"
 
 directory polignu_folder do
+  owner user
+  group user
+  mode '755'
+  action :create
+end
+
+directory confs_folder do
   owner user
   group user
   mode '755'
@@ -58,7 +66,7 @@ ssl_public_port = node['ssl_public_port']
 
 ssl_public_port = DEFAULT_SSL_PUBLIC_PORT if ssl_public_port.nil?
 
-template "#{polignu_folder}/nginx_polignu.conf" do
+template "#{confs_folder}/nginx_polignu.conf" do
   mode '644'
   owner user
   group user
@@ -71,7 +79,7 @@ template "#{polignu_folder}/nginx_polignu.conf" do
 end
 
 link '/etc/nginx/sites-enabled/polignu.conf' do
-  to "#{polignu_folder}/nginx_polignu.conf"
+  to "#{confs_folder}/nginx_polignu.conf"
 end
 
 file '/etc/nginx/sites-enabled/default' do
@@ -100,6 +108,41 @@ execute 'apt-get update'
 package 'hhvm'
 service 'hhvm' do
   action :start
+end
+
+template "#{confs_folder}/hhvm.php.ini" do
+  mode '644'
+  owner user
+  group user
+  source 'hhvm.php.ini.erb'
+  variables(
+    memory_limit: node['hhvm']['php']['memory_limit'],
+    post_max_size: node['hhvm']['php']['post_max_size'],
+    upload_max_filesize: node['hhvm']['php']['upload_max_filesize'],
+    debug_mode: node['hhvm']['php']['debug_node']
+  )
+end
+
+link '/etc/hhvm/php.ini' do
+  to "#{confs_folder}/hhvm.php.ini"
+end
+
+template "#{confs_folder}/hhvm.server.ini" do
+  mode '644'
+  owner user
+  group user
+  source 'hhvm.server.ini.erb'
+  variables(
+    hhvm_socket_file: node['hhvm']['server']['socket_file']
+  )
+end
+
+link '/etc/hhvm/server.ini' do
+  to "#{confs_folder}/hhvm.server.ini"
+end
+
+service 'hhvm' do
+  action :restart
 end
 
 #################
